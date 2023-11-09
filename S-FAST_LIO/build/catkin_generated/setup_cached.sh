16a11587be5d261166b32cbf3585da7574b5eb11
kahowang <467673523@qq.com>
2023-11-09 22:16:06 +0800

    1.download by s-faslio 2.fix for avia HeLiPR  dataset

---
+++ S-FAST_LIO/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export ROSLISP_PACKAGE_DIRECTORIES='/home/lory/s-fastlio_ws/src/S-FAST_LIO/build/devel/share/common-lisp'
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/lory/s-fastlio_ws/src/S-FAST_LIO/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/opt/ros/melodic/lib:/usr/local/tbb-2019_U8/lib:/opt/MVS/lib/64:/opt/MVS/lib/32'
+export PKG_CONFIG_PATH='/opt/ros/melodic/lib/pkgconfig:/usr/local/lib/pkgconfig'
+export PWD='/home/lory/s-fastlio_ws/src/S-FAST_LIO/build'
+export ROS_PACKAGE_PATH="/home/lory/s-fastlio_ws/src/S-FAST_LIO:$ROS_PACKAGE_PATH"
